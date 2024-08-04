5a27b8cf12afd545649cca3c461f3465f7bf4296
alexandrepayumo <alexpayumo@hotmail.com>
2024-08-04 01:33:09 -0400

    secrets uploaded

--- pages/api/create-event.js
+++ pages/api/create-event.js
@@ -1,6 +1,4 @@
 import { google } from 'googleapis';
-import { readFileSync } from 'fs';
-import path from 'path';
 import { DateTime } from 'luxon'; // Assuming you're using Luxon for date-time manipulation
 
 export default async function handler(req, res) {
@@ -16,8 +14,19 @@ export default async function handler(req, res) {
       const user = session.user;
 
       const SCOPES = ['https://www.googleapis.com/auth/calendar'];
-      const serviceAccountPath = path.join(process.cwd(), process.env.GOOGLE_SERVICE_ACCOUNT_FILE);
-      const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf-8'));
+
+      const serviceAccount = {
+        type: process.env.GOOGLE_SERVICE_ACCOUNT_TYPE,
+        project_id: process.env.GOOGLE_SERVICE_ACCOUNT_PROJECT_ID,
+        private_key_id: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
+        auth_uri: process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_URI,
+        token_uri: process.env.GOOGLE_SERVICE_ACCOUNT_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_PROVIDER_CERT_URL,
+        client_x509_cert_url: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_CERT_URL,
+      };
 
       const auth = new google.auth.GoogleAuth({
         credentials: serviceAccount,
